ce83a2ef55c5c2812c0ab21c4248c608e557b721
CHIANGCHIUTI <sc510377@gmail.com>
2017-03-31 15:44:51 +0800

    data shift function

--- CNN_autoencoder.py
+++ CNN_autoencoder.py
@@ -9,12 +9,6 @@ import matplotlib.pyplot as plt
 from glob import glob
 # from sys import exit
 from math import sqrt
-'''
-todo:
-	normalization
-	batch_normalize
-
-'''
 
 
 class CNN_autoencoder:
@@ -102,7 +96,7 @@ class CNN_autoencoder:
 				bias['deconv3'],
 				output_shape_of_dconv3
 			)
-			# deconv3 = tf.nn.sigmoid(deconv3)
+			#deconv3 = tf.nn.tanh(deconv3)
 			endecoder_output = deconv3
 			print('endecoder_output output shape :%s' % endecoder_output.get_shape())
 
@@ -171,7 +165,7 @@ class CNN_autoencoder:
 			else:
 				return tf.nn.batch_normalization(x, pop_mean, pop_var, beta, scale, 1e-3)
 
-		self.learning_rate = 0.0001
+		self.learning_rate = 0.001
 		self.training_iters = 20000
 		self.batch_size = 100
 		self.display_step = 50
@@ -286,8 +280,8 @@ class CNN_autoencoder:
 		with tf.Session() as sess:
 			self._reload_model(sess)
 			#input_x_length = input_x.shape[0]
-			loss, predict_list = self._testing_data(sess,testing_x,testing_y)
-			predict_y = np.stack(predict_list)
+			loss, predict_y = self._testing_data(sess,testing_x,testing_y)
+			
 			print('predict finished!')
 			print('loss:{} predict_y shape{}'.format(loss,predict_y.shape))
 
@@ -390,48 +384,50 @@ class CNN_autoencoder:
 
 		record = np.stack(record_list)
 		result = np.stack(result_list)
-		return index,record,result
+		return index, record, result
 
-	def _testing_data(self,sess,input_x,input_y):
-		batch_num = int(input_x.shape[0]/self.batch_size)
+	def _testing_data(self, sess, input_x, input_y):
+		batch_num = int(input_x.shape[0] / self.batch_size)
 		testing_data_number = input_y.shape[0]
-		if  testing_data_number % self.batch_size is not 0:
+		if testing_data_number % self.batch_size is not 0:
 			batch_len = batch_num + 1
 		else:
 			batch_len = batch_num
 
 		with tf.device('/gpu:0'):
-			predict_list =[]
+			predict_list = []
 			cum_loss = 0
 			for batch_index in range(batch_len):
-				
-				loss,predict = sess.run([self.cost_OP,self.endecoder_OP],feed_dict={
-						self.Xs:input_x[batch_index*self.batch_size:(batch_index+1)*self.batch_size],
-						self.Ys:input_y[batch_index*self.batch_size:(batch_index+1)*self.batch_size],
-						self.keep_prob:1,
-						self.norm:0
+				loss, predict = sess.run([self.cost_OP, self.endecoder_OP], feed_dict={
+					self.Xs: input_x[batch_index * self.batch_size:(batch_index + 1) * self.batch_size],
+					self.Ys: input_y[batch_index * self.batch_size:(batch_index + 1) * self.batch_size],
+					self.keep_prob: 1,
+					self.norm: 0
 				})
-				predict = self.un_normalize_data(sess,predict)
-				un_normalize_input_y = self.un_normalize_data(sess,input_y[batch_index*self.batch_size:(batch_index+1)*self.batch_size])
-				
+				predict = self.un_normalize_data(sess, predict)
+				un_normalize_input_y = self.un_normalize_data(sess, input_y[batch_index * self.batch_size:(batch_index + 1) * self.batch_size])
+
 				for i in range(5):
 					for j in range(predict.shape[1]):
-						print('predict:{},real:{}'.format(predict[i,j,10,20,0],un_normalize_input_y[i,j,10,20,0]))
-				
-				
+						print('predict:{},real:{}'.format(predict[i, j, 10, 20, 0], un_normalize_input_y[i, j, 10, 20, 0]))
+
 				for predict_element in predict:
 					predict_list.append(predict_element)
 				cum_loss += loss
-			return loss/batch_num, predict_list
-	def _save_history(self,input_data):
-		with open ('history.txt','w') as f:
-			f.write('{}\t{}\t{}\n'.format('epoch','training_lostt','testing_loss'))
-			for i, epoch in enumerate(input_data['epoch']):
-				f.write('{}\t{}\t{}\n'.format(input_data['epoch'][i],
-					input_data['training_loss_his'][i],
-					input_data['testting_loss_hist'][i]))
+			return cum_loss / batch_num, np.stack(predict_list)
 
-		
+	def _save_history(self, input_data):
+
+		with open('history.txt', 'w') as f:
+			f.write('{}\t{}\t{}\n'.format('epoch', 'training_lostt', 'testing_loss'))
+			for i, epoch in enumerate(input_data['epoch']):
+				f.write('{}\t{}\t{}\n'.format(
+					input_data['epoch'][i],
+					input_data['training_loss_his'][i],
+					input_data['testting_loss_hist'][i])
+				)
+
+
 	def training_data(self,restore=False):
 
 		data = self._read_data_from_Tfrecord(self.training_file)
@@ -518,7 +514,7 @@ def save_array(x_array,out_file):
 	np.save(out_file,x_array, allow_pickle=True)
 def load_array(input_file):
 	print('loading file from {}...'.format(input_file))
-	X = np.load(input_file+'.npy')
+	X = np.load(input_file)
 	return X
 
 
@@ -526,67 +522,87 @@ def load_data_format(input_dir, filelist):
 	def load_array(input_file):
 		print('loading file from {}...'.format(input_file))
 		X = np.load(input_file)
-		return X
+		return X.astype(np.float32)
 
 	def split_array(data_array):
-		#print('data_array shape :', data_array.shape)
+		# print('data_array shape :', data_array.shape)
 		split_block_size = 6  # one hour
 		data_array_depth = data_array.shape[0]
+		remainder = data_array_depth % split_block_size
 		split_block_num = int(data_array_depth / split_block_size)
 
 		# new_data_array_size = [split_block_num,data_array.shape[1:]]
 		# print('new_data_array_size:',new_data_array_size)
 
-		split_data_list = np.split(data_array, split_block_num)
+		split_data_list = np.split(data_array[:data_array_depth - remainder], split_block_num)
 		new_data_array = np.stack(split_data_list, axis=0)
-		#print('new_data_array shape:', new_data_array.shape)
+		# print('new_data_array shape:', new_data_array.shape)
 
 		return new_data_array
 
-	def array_concatenate(x, y):
+	def shift_data(data_array):
+
+		array_list = []
+		for shift_index in range(3):
+			shift_array = data_array[shift_index:]
+			array_list.append(split_array(shift_array))
+
+		return array_list
+
+	def array_concatenate(x, y):  # for reduce
 		return np.concatenate((x, y), axis=0)
 
-	array_list = []
-	for file_name in filelist:
-		data_array = load_array(input_dir + file_name)
-		array_list.append(split_array(data_array))
-	X = np.concatenate(array_list)
-	#X = fn.reduce(array_concatenate, array_list)
-	print('data format shape:', X.shape)
-	return X
+	month, _ = os.path.split(input_dir)
+	month = month.split('/')[-2]
 
-if __name__ == '__main__':
-	"""
+	data_array_list = []
+	for file_name in filelist:
+		data_array_list.append(load_array(input_dir + file_name))
+	data_array = np.concatenate(data_array_list, axis=0)
+	del data_array_list
+	shift_data_array_list = shift_data(data_array)
+	for i, array in enumerate(shift_data_array_list):
+			print('data format shape:', array.shape)
+			save_array(array, './npy/' + month + '_' + str(i))  # saving all shift array
+
+
+def prepare_training_data():
+	'''	
 	input_dir_list = [
 		"/home/mldp/big_data/openbigdata/milano/SMS/11/data_preproccessing_10/",
-		"/home/mldp/big_data/openbigdata/milano/SMS/12/data_preproccessing_10/"
-		]
+		"/home/mldp/big_data/openbigdata/milano/SMS/12/data_preproccessing_10/"]
 
-	
-	
-	X_array = None
 	for input_dir in input_dir_list:
 		filelist = list_all_input_file(input_dir)
 		filelist.sort()
-		temp = load_data_format(input_dir,filelist)
-		try:
-			X_array = np.concatenate((X_array,temp),axis=0)
-		except:
-			X_array = temp
+		load_data_format(input_dir, filelist)
+	'''
+	filelist = list_all_input_file('./npy/')
+	filelist.sort()
+	for i, filename in enumerate(filelist):
+		if filename != 'training_raw_data.npy':
+			data_array = load_array('./npy/' + filename)
+			data_array = data_array[:, :, 0:40, 0:40, -1, np.newaxis]  # only network activity
+			print('saving  array shape:{}'.format(data_array.shape))
+			save_array(data_array, './npy/final/training_raw_data' + '_' + str(i))
 
-	X_array = X_array[:, :, 30:70, 30:70, -1, np.newaxis]
-	save_array(X_array,'./proccessed_raw_data')
-	"""
 
-	
-	X_array = load_array('./proccessed_raw_data')
+if __name__ == '__main__':
+	# prepare_training_data()
+
+	training_data_list = list_all_input_file('./npy/final/')
+	training_data_list.sort()
+	X_array_list = []
+	for filename in training_data_list:
+		X_array_list.append(load_array('./npy/final/' + filename))
+
+	X_array = np.concatenate(X_array_list, axis=0)
+	del X_array_list
 	network_parameter = {'conv1': 16, 'conv2': 32, 'conv3': 48}
-	data_shape = [X_array.shape[1],X_array.shape[2],X_array.shape[3],X_array.shape[4]]
+	data_shape = [X_array.shape[1], X_array.shape[2], X_array.shape[3], X_array.shape[4]]
 	train_CNN = CNN_autoencoder(*data_shape, **network_parameter)
-	#train_CNN.reload_tfrecord('./training.tfrecoeds','./testing.tfrecoeds')
-	train_CNN.set_model_name('/home/mldp/ML_with_bigdata/output_model/CNN_autoencoder_onlyinternet_16_32_48.ckpt','/home/mldp/ML_with_bigdata/output_model/CNN_autoencoder_onlyinternet_16_32_48.ckpt')
+	# train_CNN.reload_tfrecord('./training.tfrecoeds','./testing.tfrecoeds')
+	train_CNN.set_model_name('/home/mldp/ML_with_bigdata/output_model/CNN_autoencoder_onlyinternet_16_32_48.ckpt', '/home/mldp/ML_with_bigdata/output_model/CNN_autoencoder_onlyinternet_16_32_48.ckpt')
 	train_CNN.set_training_data(X_array)
 	del X_array
-	train_CNN.training_data(restore=True)
-	#train_CNN.predict_data(X_array[int(9*X_array.shape[0]/10):])
-	
+	train_CNN.training_data(restore=False)
